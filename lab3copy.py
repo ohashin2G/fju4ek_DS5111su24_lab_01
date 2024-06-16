506467321a774dfaaa3447d1c85ea05aac10402c
fju4eK <ohashin2@gmail.com>
2024-06-15 23:55:15 -0400

    renamed lab3.py to lab3copy.py

--- lab3copy.py
+++ lab3copy.py
@@ -13,10 +13,10 @@ as keys, and their counts as value
 from collections import Counter
 import re
 from string import punctuation
+import wget
 
-sample_text = """
-And he looked over at the alarm clock, ticking on the chest of drawers. "God in Heaven!" he thought. It was half past six and the hands were quietly moving forwards, it was even later than half past, more like quarter to seven. Had the alarm clock not rung? He could see from the bed that it had been set for four o'clock as it should have been; it certainly must have rung. Yes, but was it possible to quietly sleep through that furniture-rattling noise? True, he had not slept peacefully, but probably all the more deeply because of that.
-"""
+sample_text = wget.download('https: // www.gutenberg.org/cache/epub/17192/pg17192.txt')
+#sample_text = get_the_books.sh
 
 def clean_text(sample_text):
     """
