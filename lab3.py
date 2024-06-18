f2c27401ac4257e0013eb7dcfe48320886f5412b
ohashin2G <98792149+ohashin2G@users.noreply.github.com>
2024-06-18 01:35:18 -0400

    Revert "merge to project_gutenberg_texts branch"

--- lab3.py
+++ lab3.py
@@ -13,9 +13,10 @@ as keys, and their counts as value
 from collections import Counter
 import re
 from string import punctuation
-import wget
 
-sample_text = 'philosophical prose poem of "Eureka," which he deemed the crowning work'
+sample_text = """
+And he looked over at the alarm clock, ticking on the chest of drawers. "God in Heaven!" he thought. It was half past six and the hands were quietly moving forwards, it was even later than half past, more like quarter to seven. Had the alarm clock not rung? He could see from the bed that it had been set for four o'clock as it should have been; it certainly must have rung. Yes, but was it possible to quietly sleep through that furniture-rattling noise? True, he had not slept peacefully, but probably all the more deeply because of that.
+"""
 
 def clean_text(sample_text):
     """
@@ -26,20 +27,13 @@ def clean_text(sample_text):
 		text (str): the sample text to be cleaned
 
 	Returns:
-		words: all lowercase words with no puctuation
+		words: where each item is a word in the file
 	"""
-    assert isinstance(
-        sample_text, str), f"expected str but got {type(sample_text)}"
-
     trans = str.maketrans('', '', punctuation)
-    assert not isinstance(trans, int)
-
     clean_text = sample_text.lower().translate(trans)
-    assert isinstance(clean_text, str)
-    assert not clean_text == "" , "should not be empty"
-
     return clean_text
 
+print(clean_text(sample_text))
 
 def tokenize(clean_text):
     """
@@ -50,8 +44,9 @@ def tokenize(clean_text):
 		list: a python list, where each item is a word in the file
  	"""
     tokens = re.findall(r'\b\w+\b', clean_text)
-    assert isinstance(tokenize(clean_text), list), f'Tokenizer failed on sample text: {clean_text}'
     return tokens
+
+print(tokenize(clean_text(sample_text)))
 
 def count_words(clean_text):
     """
@@ -63,20 +58,27 @@ def count_words(clean_text):
 	Returns:
 		dict: a dictionary with the words as keys, and their counts as value
 	"""
-
-    assert isinstance(
-        clean_text, int), f"expected int but got {type(clean_text)}"
-
+    
     txt = clean_text
-    assert not txt == "" , "should not be empty"
-
     count_words = Counter(txt.split())
-    assert isinstance(count_words, dict)
-
-    count_words.most_common(2)
-    assert isinstance(count_words.most_common(2), list)
-
+    count_words.most_common(2) 
     sum(count_words.values())
-    assert isinstance(sum(count_words.values()), int)
-
     return count_words
+
+print(count_words(clean_text(sample_text)))
+print(count_words(clean_text(sample_text)).most_common(2))
+print(sum(count_words(clean_text(sample_text)).values()))
+
+# Alternatively using loop
+for word, count in count_words(clean_text(sample_text)).items():
+	print(word, count)
+
+
+# Check inputs to functions
+assert isinstance(clean_text(sample_text), str)
+assert isinstance(tokenize(clean_text(sample_text)), list)
+assert isinstance(count_words(sample_text), dict)
+
+# Check return types
+
+
